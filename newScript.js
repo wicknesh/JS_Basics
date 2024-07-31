1fca98efee71902d882054d5f5391c31b3b18f4b
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-07-31 20:08:00 +0530

    First commit

---
+++ newScript.js
@@ -0,0 +1,79 @@
+// var arr2 = [{
+//     fname : 'Maruti',
+//     lname : 'New'},
+//     {
+//         fname : 'Hyundai',
+//         lname : 'Old'}
+// ]
+
+// console.log(arr2[1].lname);
+
+// function display(fname) {
+//     console.log('Welcome '+fname+', to my world');
+// }
+
+// let fname = '';
+// display(fname);
+
+// function multiply(a, b){
+//     return a * b;
+// }
+
+// console.log(multiply(5, 7));
+
+// var x = 7;
+// var y = ++x;
+// console.log(x);
+// console.log(y);
+
+// var x = (4*7)-8**2+9;
+// console.log(x);
+
+// if (0 === false){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// let exp = 53;
+// var result1 = exp>0?'value is greater than 0': 'value is less than or equal to 0';
+// console.log(result1);
+
+// for(i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+//var array = [1,2,3,4,5,6,7];
+// for(i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+
+// var obj = {
+//     fname : 'Vignesh',
+//     lname : 'J S',
+//     place : 'Trivandrum'
+// };
+
+// for (const i in obj) {
+//     console.log(obj[i]);
+// }
+
+// for (const i of array) {
+//     console.log(i);
+// }
+// let i = 0;
+// while (i<10) {
+//     console.log(i+1);
+//     i++;
+// }
+
+// do {
+//     console.log(i+1);
+//     i++;
+// } while (i<10);
+
+let arr = [1,2,3,4,5];
+for (const i of arr) {
+    console.log(i);
+}
